c46bec90d33143b5434da0a5c65f6631292960ed
QuinnBitz <99677023+QuinnBitz@users.noreply.github.com>
2023-12-17 16:46:56 +0100

    feat:add_shooting

---
+++ SpaceShooter/Source/Game/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using SpaceShooter.Source.Core;
+using SpaceShooter.Source.Core.Components;
+using SpaceShooter.Source.Core.ScriptComponent;
+using SpaceShooter.Source.Core.Utils;
+
+namespace SpaceShooter.Source.Game;
+internal class Bullet : Component, IUpdate {
+    private const float SPEED = 500;
+
+    public void Update(GameTime gameTime) {
+        Transform.position.Y -= 1 * SPEED * Time.deltaTime;
+
+        //destroy self once it is no-longer visible
+        if (Transform.position.Y < 0) {
+            GameObject.Dispose();
+        }
+    }
+}
---
+++ SpaceShooter/Source/Game/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using SpaceShooter.Source.Core;
+using SpaceShooter.Source.Core.Components;
+using SpaceShooter.Source.Core.ScriptComponent;
+using SpaceShooter.Source.Core.Utils;
+using System;
+
+namespace SpaceShooter.Source.Game;
+internal class Shooting : Component, IUpdate {
+    private const int DELAY_MILISECONDS = 1000;
+    private TimeSpan _timedOutTill = TimeSpan.Zero;
+
+    public void Update(GameTime gameTime) {
+        if (gameTime.TotalGameTime < _timedOutTill) {
+            return;
+        }
+
+        {
+            GameObject bullet = new();
+            bullet.Transform.position = Transform.position;
+            SpriteRenderer spriteRenderer = bullet.AddComponent<SpriteRenderer>();
+            spriteRenderer.spriteData.textureData.name = "spaceship/spaceship_0";
+            bullet.AddComponent<Bullet>();
+        }
+
+        _timedOutTill = gameTime.TotalGameTime + (TimeSpan.FromMilliseconds(DELAY_MILISECONDS) / Time.timeScale);
+    }
+}
