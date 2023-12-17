ee4a031ad5b91cb4892e93cee39fd85fa8f86b13
QuinnBitz <99677023+QuinnBitz@users.noreply.github.com>
2023-12-17 06:38:36 +0100

    fix:spinner
    spinner was spinning too slow and still had some rubbish from when it was used as a test component.

--- SpaceShooter/Source/Game/Spinner.cs
+++ SpaceShooter/Source/Game/Spinner.cs
@@ -5,12 +5,8 @@ using SpaceShooter.Source.Core.Utils;
 using System;
 
 namespace SpaceShooter.Source.Game;
-internal class Spinner : Component, IUpdate, IInitialize {
-    public void Initialize() {
-        Transform.position = Vector2.One * 5;
-    }
-
+internal class Spinner : Component, IUpdate {
     public void Update(GameTime gameTime) {
-        Transform.rotation += MathF.PI / 180 * 0.1f * Time.GetDeltaTime(gameTime);
+        Transform.rotation += MathF.PI / 180 * 20 * Time.GetDeltaTime(gameTime);
     }
 }
