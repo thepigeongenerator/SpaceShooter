ffa148e903c84f5b775681e788cc9da7db7bcfd9
QuinnBitz <99677023+QuinnBitz@users.noreply.github.com>
2023-12-17 03:44:19 +0100

    feat:added_animator

---
+++ SpaceShooter/Source/Core/Components/Animator.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SpaceShooter.Source.Core.Data;
+using SpaceShooter.Source.Core.ScriptComponent;
+using System;
+using System.Collections.Generic;
+
+namespace SpaceShooter.Source.Core.Components;
+
+internal class Animator : Component, IUpdate, ILoadContent {
+    //public fields
+    public int delayMiliSeconds = 42;           //~24 fps
+    public bool enabled = true;                 //whether the animator is enabled or not
+    public Func<bool> condition = () => true;   //a potential condition to disable the animator more dynamically
+    public List<TextureData> frames = new();    //a collection of the frames of the animator
+
+    //private fields
+    private TimeSpan _timedOutTill = TimeSpan.Zero;
+    private SpriteRenderer _spriteRenderer;
+    private int _frameIndex = 0; //the index of the frame we're on
+
+    public void LoadContent() {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        GameManager game = GameManager.Instance;
+        for (int i = 0; i < frames.Count; i++) {
+            TextureData frame = frames[i];
+            frame.texture2D = game.Content.Load<Texture2D>(frames[i].name);
+            frames[i] = frame;
+        }
+    }
+
+    //called every frame update
+    public void Update(GameTime gameTime) {
+        //conditions to pass to run the animator
+        if (enabled && condition.Invoke() && gameTime.TotalGameTime < _timedOutTill) {
+            return;
+        }
+
+        //timeout over
+        _spriteRenderer.spriteData.textureData = frames[_frameIndex]; //swap out the current texture with the texture in the animator
+
+        _frameIndex++; //incriment the frame index by 1
+
+        //set the frameIndex back to 0 if it becomes out of bounds
+        if (_frameIndex >= frames.Count) {
+            _frameIndex = 0;
+        }
+
+        //reset the timeout
+        _timedOutTill = gameTime.TotalGameTime + TimeSpan.FromMilliseconds(delayMiliSeconds);
+    }
+}
